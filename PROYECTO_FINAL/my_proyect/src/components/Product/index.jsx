d8232d324fc2923ba9b173b6b1a0c5a6a2e2f3b6
Nicol Tapasco <tapasconicol25@gmail.com>
2024-08-14 13:02:16 -0500

    proyecto final

--- PROYECTO_FINAL/my_proyect/src/components/Product/index.jsx
+++ PROYECTO_FINAL/my_proyect/src/components/Product/index.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {CartContext} from "../Context/CartContext"
 import styles from './Product.module.css';
 
 
 const Product = ({ id, image, title, description, price, rating, onClick }) => {
+  const { addToCart } = useContext(CartContext);
+
+  const handleClick = () => {
+    const product = { image, title, description, price, rating, id: title }; // Aquí asumimos que el título es único para simplificar
+    addToCart(product);
+  };
   return (
     <div className={styles.productCard}>
       <img className={styles.productImage} src={image} alt={title} />
@@ -15,7 +22,7 @@ const Product = ({ id, image, title, description, price, rating, onClick }) => {
         </div>
       </div>
       <div className={styles.div_btnComprar}>
-        <button onClick={onClick} className={styles.btnComprar}>Comprar</button>
+        <button onClick={handleClick} className={styles.btnComprar}>Comprar</button>
       </div>
     </div>
   );
